717852635fc24a10b77463c89b1c5d7d4a08c87d
Andrey Prisniak <aprisniak@lectrum.io>
2021-05-31 17:32:15 +0300

    feat(source): fix promise example

--- src/examples/jest/6/index.test.js
+++ src/examples/jest/6/index.test.js
@@ -1,8 +1,5 @@
 const message = 'resolved!';
 
-// TODO:
-// jest.useFakeTimers();
-
 const delay = (duration = 1000) => {
     return new Promise((resolve) => {
         setTimeout(() => resolve(message), duration);
@@ -16,7 +13,3 @@ test('old: «delay» function should return a promise', () => {
 test('new: «delay» function should return a promise', async () => {
     await expect(delay()).resolves.toBe(message);
 });
-
-// test('very old: «delay» function should return a promise', async () => {
-//     expect(await delay()).toBe(message);
-// });
