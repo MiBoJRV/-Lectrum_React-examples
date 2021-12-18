5e446ba891cc8f6f17a767f3fafbcde4100d4528
Anton <akhudiakov97@gmail.com>
2021-12-18 12:11:50 +0000

    Updated swapi url

--- src/examples/redux-thunk/1/api/index.js
+++ src/examples/redux-thunk/1/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = Object.freeze({
     fetchPlanets: (page = 1) => {
-        const data = axios.get(`https://swapi.dev/api/planets/?page=${page}`);
+        const data = axios.get(`https://swapi.tech/api/planets/?page=${page}`);
 
         return data;
     },
